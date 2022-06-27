e8ad5b32218317e7d8ea43a2e6451d2929951292
Ruben Paredes <ruben.pardes25@gmail.com>
2022-06-27 11:14:29 -0400

    Commit to trigger Heroku auto deploy

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -9,7 +9,7 @@ const register: ServerRoute = {
   },
   handler: (request) => {
     const db = new NotesProjectDataSource()
-    console.log("auth.controller.ts ~ request.payload", request.payload)
+    console.log("auth.controller.ts ~ request.payload", request.payload);
     return db.register(request.payload as UserContent)
   },
 }
