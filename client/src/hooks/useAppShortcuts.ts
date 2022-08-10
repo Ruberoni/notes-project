d6840f855ddc5e43df36731e6e36bf34cece00c4
Ruben Paredes <ruben.pardes25@gmail.com>
2022-08-10 15:56:21 -0400

    Create useAppShortcuts

---
+++ client/src/hooks/useAppShortcuts.ts
@@ -0,0 +1,17 @@
+// import { useHotkeys } from 'react-hotkeys-hook'
+
+const SHORTCUTS = {
+  CREATE_NOTE: 'ctrl+shift+c',
+  FOCUS_SEARCH: 'ctrl+k',
+  FOCUS_NOTE_EDITOR: 'ctrl+shift+e',
+} 
+
+
+const useAppShortcuts = () => {
+}
+
+const useNoteContentShortcuts = () => {
+  // return useHotkeys()
+}
+
+export default useAppShortcuts
